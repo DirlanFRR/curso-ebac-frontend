7d2824659bfa9cff7cf9d2114087b07d33e1ecc0
Dirlan Fernando <dirlanfr@gmail.com>
2023-12-02 19:39:03 -0300

    typescript

---
+++ typescript2/exerciciots.js
@@ -0,0 +1,14 @@
+"use strict";
+// Função de multiplicação
+function multiplicarNumeros(a, b) {
+    return a * b;
+}
+// Função de saudação
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+// Exemplos de uso
+const resultadoMultiplicacao = multiplicarNumeros(10, 1);
+const mensagemSaudacao = saudacao("Dirlan");
+console.log(resultadoMultiplicacao); // Saída: 15
+console.log(mensagemSaudacao); // Saída: Olá Usuário
